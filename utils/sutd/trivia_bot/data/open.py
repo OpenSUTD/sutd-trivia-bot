f78c3fef7415928f293ba1d570cc1fb7f247599d
Lee Gui An <piroton@users.noreply.github.com>
2020-09-10 18:51:58 +0800

    Update open.py

--- utils/sutd/trivia_bot/data/open.py
+++ utils/sutd/trivia_bot/data/open.py
@@ -37,4 +37,12 @@ questions = [
         "question": """In outreach workshops, the Office of Marketing often sells the "hacker culture" in SUTD by showing a historical photograph of students placing a certain object on the root. What was it?""",
         "answer": "Police Car",
     },
+    {'question': 'Before the current SUTD Hostel, student housing was done differently. What was this Housing programme called?', 'answer': '6ixdegrees'},
+    {'question': "During the 10 years' anniversary event, Marketing deployed a piloted robot carrying rings that would light up a sculpture. What is the sculpture of?", 'answer': 'Tree'},
+    {'question': 'For the launch of SUTD Academy, there was a car model placed on the pedestal which had a car model number on its side. What is this model number?', 'answer': 'X18'}, 
+    {'question': 'Where is the first campus of SUTD located?', 'answer': 'Commonwealth'},
+    {'question': 'There are two things referred to as "SAC" in SUTD; one of them is a council. The other is a __?', 'answer': 'Room'},
+    {'question': 'There is a light water vehicle with pontoons under the stairs at building 2, made for a competition. What was this competition?', 'answer': 'RobotX'},
+    {'question': "SUTD's Architecture and Sustainable Design has traditionally helped out with an installation for a specific holiday. Which holiday is this?", 'answer': 'Chinese New Year'},
+    {'question': 'How many crossings, not including the ground floor, go between two separate school buildings in SUTD (as of 2020)?', 'answer': '5'}
 ]
